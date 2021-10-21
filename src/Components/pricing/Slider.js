8d195c24e91c4fa5d8db48c7940f847d81ad591a
Tejeshwer Singh Sachdeva <tejeshwersingh2504@gmail.com>
2021-10-21 13:17:25 +0530

    Toggle Button
    - Created the functional toggle Button

--- src/Components/pricing/Slider.js
+++ src/Components/pricing/Slider.js
@@ -1,4 +1,8 @@
 import React, {useState} from 'react';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { styled } from '@mui/material/styles';
+
 import './Slider.css'
 
 const Slider = () => {
@@ -6,6 +10,7 @@ const Slider = () => {
     const [subscriptionType, setSubscriptionType] = useState('month');
     const [views, setViews] = useState('1');
     const [cost, setCost] = useState('16');
+    const [checked, setChecked] = useState('false');
 
     const handleViewChange = (e) => {
         let tempViews = e.target.value;
@@ -13,13 +18,64 @@ const Slider = () => {
         setCost(subscriptionType==='month' ? (tempViews*16) : ((tempViews*16*12)*(75/100)))
     }
 
-    const handleSubscriptionChange = (e) => {
-        let checked = e.target.checked;
-        setSubscriptionType(!checked ? 'month' : 'year');
+    const handleSubscriptionChange = () => {
+        setChecked(!checked);
+        setSubscriptionType(checked ? 'month' : 'year');
 
-        let tempcost = checked ? (views*16*12)*(75/100) : (views*16); 
+        let tempcost = !checked ? (views*16*12)*(75/100) : (views*16); 
         setCost(tempcost);
     }
+
+    const IOSSwitch = styled((props) => (
+        <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+      ))(({ theme }) => ({
+        width: 42,
+        height: 26,
+        padding: 0,
+        '& .MuiSwitch-switchBase': {
+          padding: 0,
+          margin: 2,
+          transitionDuration: '300ms',
+          '&.Mui-checked': {
+            transform: 'translateX(16px)',
+            color: '#fff',
+            '& + .MuiSwitch-track': {
+              backgroundColor: theme.palette.mode === 'dark' ? '#A5F3EB' : '#10d5c2',
+              opacity: 1,
+              border: 0,
+            },
+            '&.Mui-disabled + .MuiSwitch-track': {
+              opacity: 0.5,
+            },
+          },
+          '&.Mui-focusVisible .MuiSwitch-thumb': {
+            color: '#A5F3EB',
+            border: '6px solid #fff',
+          },
+          '&.Mui-disabled .MuiSwitch-thumb': {
+            color:
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[600],
+          },
+          '&.Mui-disabled + .MuiSwitch-track': {
+            opacity: theme.palette.mode === 'light' ? 0.7 : 0.3,
+          },
+        },
+        '& .MuiSwitch-thumb': {
+          boxSizing: 'border-box',
+          width: 22,
+          height: 22,
+        },
+        '& .MuiSwitch-track': {
+          borderRadius: 26 / 2,
+          backgroundColor: theme.palette.mode === 'light' ? '#E9E9EA' : '#A5F3EB',
+          opacity: 1,
+          transition: theme.transitions.create(['background-color'], {
+            duration: 500,
+          }),
+        },
+      }));
 
     return (
         <div className="slider">
@@ -37,7 +93,15 @@ const Slider = () => {
 
             <div className="slider__toggle">
                 <p>Monthly Billing</p>
-                <input type="checkbox" className="slider__toggle--input" onChange={(e) => handleSubscriptionChange(e)}/>
+
+                <div className="slider__toggle--switch">
+                    <FormControlLabel
+                        control={<IOSSwitch sx={{ m: 1 }} checked={checked}  />}
+                        label=""
+                        onChange={handleSubscriptionChange}
+                    />
+                </div>
+
                 <p>Yearly Billing <span className="slider__toggle--discountChip">-25% <span>Discount</span></span></p>
             </div>
         </div>
